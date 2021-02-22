a28c9dfede690770964724789465402f142bacc9
Nischay Gowda <nischaygowda777@gmail.com>
2021-02-22 10:39:21 +0530

    Update sql.py

--- MySQL/sql.py
+++ MySQL/sql.py
@@ -1,1 +1,22 @@
+'''
+For this challenge you need to create a simple SELECT statement that will return all columns from the people table, 
+and join to the toys table so that you can return the COUNT of the toys
 
+people table schema
+ * id
+ * name
+  
+toys table schema
+ * id
+ * name
+ * people_id
+You should return all people fields as well as the toy count as "toy_count".
+'''
+
+
+-- Create your SELECT statement here
+SELECT P.id, P.name, COUNT(T.name) toy_count
+FROM people P
+-- Join on common column people id.
+JOIN toys T ON P.id = T.people_id
+GROUP BY P.id;
